bf951a41415f8f836ec8e2f0ac0034d26c644019
MPaidos <maks22012005@gmail.com>
2026-04-09 21:15:16 +0700

    3

--- prosto/prosto/settings.py
+++ prosto/prosto/settings.py
@@ -123,13 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/MPaidaKI23/ISIS/prosto/staticfiles'
-STATICFILES_DIRS = [
-    '/home/MPaidaKI23/ISIS/prosto/static',
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/MPaidaKI23/ISIS/prosto/media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
