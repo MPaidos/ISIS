f0baadb1f2bac89a941d52fa993c73ec5e724efb
MPaidos <maks22012005@gmail.com>
2026-04-09 20:53:39 +0700

    переделка статики

--- prosto/prosto/settings.py
+++ prosto/prosto/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = '/home/MPaidaKI23/ISIS/prosto/staticfiles'
+STATICFILES_DIRS = [
+    '/home/MPaidaKI23/ISIS/prosto/static',
+]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/home/MPaidaKI23/ISIS/prosto/media'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
